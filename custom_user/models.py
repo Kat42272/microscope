7499bf037e5b2e7c69d862f067b01c1f1ee4d372
Kathryn Anderson <kat42272@yahoo.com>
2020-08-24 16:58:08 -0700

    Added forms models views templates urls and migrations

--- custom_user/models.py
+++ custom_user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+# Study Hall Group assistance with this section
+# Used information from https://docs.djangoproject.com/en/3.1/topics/auth/customizing/
+# William Vincent Django For Beginners Chapter 8
+
+class CustomUser(AbstractUser):
+  age = models.PositiveIntegerField(null=True, blank=True)
--- microscope_config/settings.py
+++ microscope_config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'custom_user',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'microscope_config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'custom_user.CustomUser'
+LOGIN_REDIRECT_URL = 'home'
+LOTOUT_REDIRECT_URL = 'home'
